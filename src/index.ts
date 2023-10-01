2d66770fb8f2aaa8749d1bb9da8669c24066fc23
NilayYadav <nilayyadav10@gmail.com>
2023-10-01 13:28:30 +0530

    f

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
 import { readFile, writeFile } from "fs/promises";
 import { FFmpeg } from "@ffmpeg.wasm/main";
+import express, { Request, Response } from 'express'
+
+const app = express()
+const port = process.env.PORT || 3000
+
 
 const ffmpeg = await FFmpeg.create({ core: "@ffmpeg.wasm/core-mt" });
 
+app.get('/', (_req: Request, res: Response) => {
+    const v = ffmpeg.version.core.version
+    return res.send({ message: 'Express Typescript on Vercel', v })
+})
+
+app.get('/ping', (_req: Request, res: Response) => {
+    return res.send('pong 🏓')
+})
+
+app.listen(port, () => {
+    return console.log(`Server is listening on ${port}`)
+})
